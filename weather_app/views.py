543a688260eb16ab560f8db0c280486d4c004af8
oomat07 <shakiralievomat3@gmail.com>
2024-02-28 17:45:47 +0600

    version 1.0

--- weather_app/views.py
+++ weather_app/views.py
@@ -6,15 +6,16 @@ from aweather_project.settings import API_KEY
 def get_weather_data(city_name):
     api_key = API_KEY
     url = f'http://api.openweathermap.org/data/2.5/weather'
-    psrams = {'q':city_name, 'units':'metric', 'lang':'ru', 'APPID':api_key}
+    params = {'q':city_name, 'units':'metric', 'lang':'ru', 'APPID':api_key}
 
-    response = requests.get(url)
+    response = requests.get(url, params)
     return response.json()
 
 def index(request):
     if request.method == 'POST':
         city_name = request.POST.get('city_name')
         weather_data = get_weather_data(city_name)
+        print(weather_data)
     else:
         weather_data = None
     return render(request, 'weather_app/index.html', {'weather_data':weather_data})
